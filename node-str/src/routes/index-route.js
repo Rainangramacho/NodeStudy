90985f570afda5dcae115706d1696dcb22febf6a
Rainangramacho <rai_bomfim@hotmail.com>
2019-07-26 18:10:19 -0300

    Removing a unusable constant
    Removing the unusable constant 'route'

--- node-str/src/routes/index-route.js
+++ node-str/src/routes/index-route.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router() // criando variavel qu recebe rotas
 
 //configurando rotas
-const route = router.get ('/', (req, res, next)=>{
+router.get ('/', (req, res, next)=>{
     res.status(200).send({
         title: "Node Store API",
         version: "0.0.1"
