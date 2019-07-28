04046fc6decbca54de532acb417f7eabaf9c4f37
Rainangramacho <rai_bomfim@hotmail.com>
2019-07-28 16:10:57 -0300

    Creating CRUD for bateria

--- node-str/src/controllers/bateria-controller.js
+++ node-str/src/controllers/bateria-controller.js
@@ -14,9 +14,9 @@ exports.obter_bateria = (req, res, next) =>{
 
 exports.criar_bateria = (req, res, next) =>{
     const bateria = { 
-        id: req.body.id
+        surfista_numero: req.body.surfista_numero
       }
-    var sql = 'INSERT INTO bateria (id) VALUES (" ' + bateria.id +' ");';
+    var sql = 'INSERT INTO bateria (Surfista_numero) VALUES (" ' + bateria.surfista_numero +' ");';
     conexao.query(sql, function(err, rows, fields){
         if (err){
             res.status(500).send({error: ' Algo falhou '})
@@ -25,3 +25,40 @@ exports.criar_bateria = (req, res, next) =>{
     }) 
 };
 
+exports.editar_bateria = (req, res, next) =>{
+    const bateria = { 
+        surfista_numero_params: req.params.Surfista_numero,
+        surfista_numero_body: req.body.Surfista_numero
+      }
+    var sql = 'UPDATE bateria SET Surfista_numero = " ' + bateria.surfista_numero_body +' " WHERE Surfista_numero = " ' + bateria.surfista_numero_params + ' ";';
+    conexao.query(sql, function(err, rows, fields){
+        if (err){
+            res.status(500).send({error: ' Algo falhou '})
+        }
+        res.json(rows);
+    }) 
+};
+
+exports.obter_surfistas_na_bateria = (req, res, next) =>{
+    var sql = "SELECT surfista.nome FROM bateria INNER JOIN surfista ON (bateria.Surfista_numero = surfista.numero)";
+    conexao.query(sql, function(err, rows, fields){
+        if (err){
+            res.status(500).send({error: ' Algo falhou '})
+        }
+        res.json(rows);
+    }) 
+};
+
+exports.deletar_bateria = (req, res, next)=>{
+    const bateria = { 
+        id : req.params.id,
+      }
+
+   var sql6 = 'DELETE FROM bateria WHERE id = " ' + bateria.id + ' "; ';
+   conexao.query(sql6, function(err, rows, fields){
+    if (err){
+        res.status(500).send({error: ' Algo falhou '})
+    }
+    res.json(rows);
+}) 
+};
