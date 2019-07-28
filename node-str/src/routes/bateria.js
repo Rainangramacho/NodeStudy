4fe283977596349076693b467b8222575599e567
Rainangramacho <rai_bomfim@hotmail.com>
2019-07-28 16:10:31 -0300

    Creating new routes

--- node-str/src/routes/bateria.js
+++ node-str/src/routes/bateria.js
@@ -7,9 +7,10 @@ const controller = require('../controllers/bateria-controller');
 
 //configurando rotas
 router.get ('/obter_bateria', controller.obter_bateria);
+router.get ('/obter_surfistas_na_bateria', controller.obter_surfistas_na_bateria);
 router.post ('/criar_bateria', controller.criar_bateria);
-/*router.put ('/editar_surfista_pais/:numero', controller.editar_surfista_pais);
-router.delete ('/deletar_surfista/:numero', controller.deletar_surfista); */
+router.put ('/editar_bateria/:Surfista_numero', controller.editar_bateria);
+router.delete ('/deletar_bateria/:id', controller.deletar_bateria); 
 
 module.exports = router;
 
