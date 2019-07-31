5d3c15db48a7451e9ee1b7dd8cbef9fc54e0fdd2
Rainangramacho <rai_bomfim@hotmail.com>
2019-07-31 00:05:24 -0300

    Creating new routes

--- node-str/src/routes/notas.js
+++ node-str/src/routes/notas.js
@@ -6,8 +6,9 @@ const controller = require('../controllers/nota-controller');
 
 
 //configurando rotas
-//router.get ('/obter_nota', controller.obter_nota);
+router.get ('/visualizar_notas', controller.visualizar_notas);
 router.post ('/cadastrar_nota', controller.cadastrar_nota);
+router.get ('/surfista_vencedor', controller.surfista_vencedor);
 //router.put ('/editar_nota/:Onda_id', controller.editar_nota);
 //router.delete ('/deletar_nota/:id', controller.deletar_nota); 
 
