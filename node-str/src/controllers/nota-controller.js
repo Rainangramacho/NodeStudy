dbb962de0a207485c24b4a35087d459e59801301
Rainangramacho <rai_bomfim@hotmail.com>
2019-08-01 23:32:58 -0300

    Creating visualizar nota route

--- node-str/src/controllers/nota-controller.js
+++ node-str/src/controllers/nota-controller.js
@@ -20,7 +20,7 @@ exports.cadastrar_nota = (req, res, next) => {
 }) 
 };
 
-exports.visualizar_notas = (req, res, next) => {
+exports.visualizar_notas_parciais = (req, res, next) => {
 
     var sql = 'SELECT * FROM nota ;';
 
@@ -31,6 +31,22 @@ exports.visualizar_notas = (req, res, next) => {
     res.json(rows);
 }) 
 };
+
+exports.visualizar_notas = (req, res, next) => {
+    const nota = { 
+        onda_id: req.body.onda_id
+    }
+
+    var sql = 'Select id,((notaparcial1 + notaparcial2 + notaparcial3)/3) as Nota_surfista from nota where Onda_id = " '+ nota.onda_id +'";';// na onda informada a nota do surfista foi tal
+
+   conexao.query(sql, function(err, rows, fields){
+    if (err){
+        res.status(500).send({error: ' Algo falhou '})
+    }
+    res.json(rows);
+}) 
+};
+
 
 exports.surfista_vencedor = (req, res, next) => {
 
