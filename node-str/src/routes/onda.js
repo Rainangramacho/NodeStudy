8212ba2709b3b759bea10aa507811564b579dbcc
Rainangramacho <rai_bomfim@hotmail.com>
2019-08-08 20:29:45 -0300

    Finalizing onda route

--- node-str/src/routes/onda.js
+++ node-str/src/routes/onda.js
@@ -8,8 +8,6 @@ const controller = require('../controllers/onda-controller');
 //configurando rotas
 router.get ('/visualizar_ondas', controller.visualizar_ondas);
 router.post ('/cadastrar_onda', controller.cadastrar_onda);
-//router.put ('/editar_bateria/:Surfista_numero', controller.editar_bateria);
-//router.delete ('/deletar_bateria/:id', controller.deletar_bateria); 
 
 module.exports = router;
 
