d625f2f071a8da2489b25bcb053ab0d5f2c305e1
Rainangramacho <rai_bomfim@hotmail.com>
2019-08-08 20:30:29 -0300

    Finalizing bateria route

--- node-str/src/routes/bateria.js
+++ node-str/src/routes/bateria.js
@@ -9,7 +9,6 @@ const controller = require('../controllers/bateria-controller');
 router.get ('/obter_bateria', controller.obter_bateria);
 router.get ('/obter_surfistas_na_bateria', controller.obter_surfistas_na_bateria);
 router.post ('/criar_bateria', controller.criar_bateria);
-//router.put ('/editar_bateria/:Surfista_numero', controller.editar_bateria);
 router.delete ('/deletar_bateria/:id', controller.deletar_bateria); 
 
 module.exports = router;
