655661107fa4e7ccae4cfe8ed40fe1c490a0cb53
Rainangramacho <rai_bomfim@hotmail.com>
2019-08-08 20:30:38 -0300

    Finalizing notas route

--- node-str/src/routes/notas.js
+++ node-str/src/routes/notas.js
@@ -7,11 +7,8 @@ const controller = require('../controllers/nota-controller');
 
 //configurando rotas
 router.get ('/visualizar_notas_parciais', controller.visualizar_notas_parciais);
-router.get ('/visualizar_notas', controller.visualizar_notas);
+router.get ('/surfista_vencedor_bateria', controller.surfista_vencedor_bateria);
 router.post ('/cadastrar_nota', controller.cadastrar_nota);
-router.get ('/surfista_vencedor', controller.surfista_vencedor);
-//router.put ('/editar_nota/:Onda_id', controller.editar_nota);
-//router.delete ('/deletar_nota/:id', controller.deletar_nota); 
 
 module.exports = router;
 
